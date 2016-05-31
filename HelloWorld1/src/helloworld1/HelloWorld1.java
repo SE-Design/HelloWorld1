10767238e91f8c614d6f52868433b3d49cc4a30b
SE-Design <kashifniazi1@yahoo.com>
2016-05-31 13:24:07 +0200

    Update HelloWorld1.java
    latest change

--- HelloWorld1/src/helloworld1/HelloWorld1.java
+++ HelloWorld1/src/helloworld1/HelloWorld1.java
@@ -12,7 +12,7 @@ package helloworld1;
 public class HelloWorld1 {
 
     /**
-     * @param args the command line arguments
+    
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
